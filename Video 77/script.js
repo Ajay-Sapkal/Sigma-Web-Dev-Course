d4c7cd11682cd366c9ace74a72e2cc44b64f8f6a
Ajay-Sapkal <ajaysapkal1000@gmail.com>
2025-07-21 22:48:05 +0530

    corrected video card creation function with

--- Video 77/script.js
+++ Video 77/script.js
@@ -1,15 +1,17 @@
+// Function to create a video card with given parameters
 function createCard(title, cName, views, monthsOld, duration, thumbnail) {
-    // Finish this function
+    // Format view count for display
     let viewStr
     if (views < 1000) {
-        viewStr = views;
+        viewStr = views; // Display as is for small numbers
     }
     else if (views > 1000000) {
-        viewStr = views / 1000000 + "M";
+        viewStr = views / 1000000 + "M"; // Convert to millions
     }
     else {
-        viewStr = views / 1000 + "K";
+        viewStr = views / 1000 + "K"; // Convert to thousands
     }
+    // Create HTML structure for the video card
     let html = `<div class="card">
     <div class="image">
         <img src="${thumbnail}"
@@ -22,10 +24,9 @@ function createCard(title, cName, views, monthsOld, duration, thumbnail) {
     </div> 
 </div>`
 
+    // Add the card HTML to the container
     document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
 }
 
-
-createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
-
-function sum 
+// Call the function to create a video card
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw") 
